17dfb589265f867e34f68f74f80b9413762025a9
JubayerCodes <jubayerhossain111220@gmail.com>
2025-04-04 10:27:08 +0600

    added about section description

--- src/Components/Home/About/About.jsx
+++ src/Components/Home/About/About.jsx
@@ -2,8 +2,28 @@ import SectionTitle from '@/Components/Shared/SectionTitle/SectionTitle';
 import React from 'react';
 import { Fade } from 'react-awesome-reveal';
 import { FaRegUser } from 'react-icons/fa';
+import cnitLogo from "@/assets/img/cnit.png"
+import softripleLogo from "@/assets/img/softriple.webp"
 
 const About = () => {
+
+    const experience = [
+        {
+            title: "MERN Stack Developer",
+            company: "CODE NEXT IT",
+            logo: cnitLogo,
+            from: "jan 2025",
+            to: "Present"
+        },
+        {
+            title: "Frontend Developer",
+            company: "SOFTRIPLE",
+            logo: softripleLogo,
+            from: "Aug 2023",
+            to: "Sep 2023"
+        },
+    ]
+
     return (
         <section className='section' id='about'>
             <SectionTitle icon={<FaRegUser />} title={"About me"} />
@@ -13,6 +33,19 @@ const About = () => {
                 <p className="description">
                     I’m Md. Jubayer Hossain, a passionate MERN Stack Developer building modern, user-friendly web applications. Through Jubayer Codes, I turn ideas into impactful digital solutions. With solid skills in React, Node.js, MongoDB, and more, I’m ready to help clients create fast, scalable, and beautiful websites that deliver results.
                 </p>
+                <div className="experience-container flex gap-5 justify-between items-start mt-5">
+                    {
+                        experience?.map((ex, idx) => (
+                            <div className='card experience-card w-1/2 flex justify-between items-start' key={idx}>
+                                <div>
+                                    <h4 className='text-base capitalize mb-3'>{ex?.title}</h4>
+                                    <img src={ex?.logo?.src} className='h-[20px] grayscale-100' alt="jubayer codes exp" />
+                                </div>
+                                <div className="text-[var(--text-color)] capitalize text-white">{ex?.from} - {ex?.to}</div>
+                            </div>
+                        ))
+                    }
+                </div>
             </Fade>
         </section>
     );
--- src/Components/Home/Introduction/Introduction.jsx
+++ src/Components/Home/Introduction/Introduction.jsx
@@ -3,27 +3,9 @@ import React from 'react';
 import { LuHouse } from "react-icons/lu";
 import "./Introduction.css"
 import { Fade } from 'react-awesome-reveal';
-import cnitLogo from "@/assets/img/cnit.svg"
-import softripleLogo from "@/assets/img/softriple.webp"
+import StarBorder from '@/Components/Shared/StarBorder/StarBorder';
 
 const Introduction = () => {
-
-    const experience = [
-        {
-            title: "MERN Stack Developer",
-            company: "CODE NEXT IT",
-            logo: cnitLogo,
-            from: "jan 2025",
-            to: "Present"
-        },
-        {
-            title: "Frontend Developer",
-            company: "SOFTRIPLE",
-            logo: softripleLogo,
-            from: "Aug 2023",
-            to: "Sep 2023"
-        },
-    ]
 
     return (
         <section className='section' id='introduction'>
@@ -39,19 +21,7 @@ const Introduction = () => {
                 <p className="description">
                     I design and code beautifully simple things and i love what i do. Just simple like that!
                 </p>
-                <div className="experience-container flex gap-5 justify-between items-start mt-5">
-                    {
-                        experience?.map((ex, idx) => (
-                            <div className='card experience-card w-1/2 flex justify-between items-start' key={idx}>
-                                <div>
-                                    <h4 className='text-base capitalize mb-3'>{ex?.title}</h4>
-                                    <img src={ex?.logo?.src} className='h-[20px] grayscale-100' alt="jubayer codes exp" />
-                                </div>
-                                <div className="text-[var(--text-color)] capitalize text-white">{ex?.from} - {ex?.to}</div>
-                            </div>
-                        ))
-                    }
-                </div>
+                <button className="btn mt-5">What I’ve Built</button>
             </Fade>
         </section>
     );
